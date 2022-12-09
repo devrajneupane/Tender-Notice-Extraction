bf1da4dc713a9c9ac208e5dc482fd9391cffe144
Devraj Neupane <asminneupane79@gmail.com>
2022-12-09 12:04:19 +0545

    database adjustments

--- source/sql.py
+++ source/sql.py
@@ -2,65 +2,32 @@ import os
 import sys
 import mysql.connector
 from pathlib import Path
-from dotenv import dotenv_values
 from log import Logger
+from utils import config
 
 sys.stdout = Logger()
 
-path = Path(__file__).parent
 
-dir_lst = os.listdir(path)
-if ".env" not in dir_lst:
-    print(f"==>\n.env file not found in: \n{path}\n<==")
-    exit()
-env_path = path.joinpath(".env")
-
-# load values form .env file
-try:
-    tesseract_exec = dotenv_values(env_path)["TESSERACT_EXECUTABLE"]
-except KeyError:
-    print("TESSERACT_EXECUTABLE not found in .env file")
-    exit()
-
-try:
-    USER_NAME = dotenv_values(env_path)["SQL_USER_NAME"]
-except KeyError:
-    print("SQL_USER_NAME not found in .env file")
-    exit()
-
-try:
-    USER_PASS = dotenv_values(env_path)["SQL_USER_PASS"]
-except KeyError:
-    print("SQL_USER_PASS not found in .env file")
-    exit()
-
-try:
-    HOST = dotenv_values(env_path)["SQL_HOST"]
-except KeyError:
-    print("SQL_HOST not found in .env file")
-    exit()
-
-
-def sql_initialize(query, is_insert):
+def sql_initialize(query, is_insert=False):
     """
-    A database named tender must be exists with a table named tenderweb_tender with following fields
-    id, image_id, date_published, newspaper_source, page_number, image_name
+    A database named tender must be exists with a table named tender_web_tender with following fields
+    id, image_id, date_published, newspaper_source, page_number, image_path
     Run following SQL query if the databse and table doesn't exist already
     CREATE DATABASE IF NOT EXISTS tender;
     USE tender;
-    CREATE TABLE IF NOT EXISTS tenderweb_tender(
+    CREATE TABLE IF NOT EXISTS tender_web_tender(
             id int AUTO_INCREMENT PRIMARY KEY,
             image_id int,
             date_published DATE,
             newspaper_source varchar(255),
             page_number int,
-            image_name varchar(255));
+            image_path varchar(255));
 
     """
     sys.stdout = Logger()
 
     try:
-        with mysql.connector.connect(host=HOST, user=USER_NAME, password=USER_PASS, database="tender") as connection:
+        with mysql.connector.connect(host=config["SQL_HOST"], user=config["SQL_USER_NAME"], password=config["SQL_USER_PASS"], database="tender") as connection:
             with connection.cursor() as cursor:
                 cursor.execute(query)
                 if not is_insert:
@@ -75,28 +42,28 @@ def sql_initialize(query, is_insert):
         return False
 
 
-def is_duplicate(image_name):
-    check_duplicate = f"SELECT * FROM tenderweb_tender WHERE image_name = '{image_name}'"
-    data = sql_initialize(check_duplicate, False)
+def is_duplicate(image_path):
+    check_duplicate = f"SELECT * FROM tender_web_tender WHERE image_path = '{image_path}'"
+    data = sql_initialize(check_duplicate)
     return True if data else False
 
 
-def sql_insert(img_id, date, newspaper, page, image_name):
+def sql_insert(img_id, date, newspaper, page, image_path):
     """
      This function inserts the data into the database:
      1) image_id: id of the image
      2) date_published: date of the tender
      3) newspaper_source: name of the newspaper
      4) page_number: page number of the newspaper where the tender was found
-     5) image_name: name of the image
+     5) image_path: name of the image
      """
     sys.stdout = Logger()
 
-    insert_query = f"INSERT INTO tenderweb_tender (image_id, date_published, newspaper_source, page_number, image_name)\
-                 VALUES ({img_id}, '{date}', '{newspaper}', {page}, '{image_name}')"
-    if not is_duplicate(image_name):
+    insert_query = f"INSERT INTO tender_web_tender (image_id, date_published, newspaper_source, page_number, image_path)\
+                 VALUES ({img_id}, '{date}', '{newspaper}', {page}, '{image_path}')"
+    if not is_duplicate(image_path):
         if sql_initialize(insert_query, True):
-            print(f"\t\t\t==>{image_name.split('/')[3]} inserted into database")
+            print(f"\t\t\t==>{image_path.split('/')[3]} inserted into database")
 
 
 def sql_query_info():
@@ -104,12 +71,17 @@ def sql_query_info():
     This function returns the date of the last tender added to the database and a boolean value as a flag
     """
 
-    date_query = "SELECT date_published FROM tenderweb_tender ORDER BY id DESC LIMIT 1"
+    date_query = "SELECT date_published FROM tender_web_tender ORDER BY id DESC LIMIT 1"
     try:
-        latest_date = sql_initialize(date_query, False)[0]
+        latest_date = sql_initialize(date_query)[0]
     except IndexError:
-        latest_date = sql_initialize(date_query, False)
+        latest_date = sql_initialize(date_query)
 
-    source_query = f"SELECT DISTINCT newspaper_source FROM tender.tenderweb_tender WHERE date_published = '{latest_date[0]}'"
-    data = [data for data in sql_initialize(source_query, False) for data in data]
-    return latest_date, 'kantipur' in data or 'kathmandupost' in data
+    # Failsafe for empty database
+    if latest_date:
+        source_query = f"SELECT DISTINCT newspaper_source FROM tender.tender_web_tender WHERE date_published = '{latest_date[0]}'"
+        data = [data for data in sql_initialize(source_query) for data in data]
+        return latest_date, 'kantipur' in data or 'kathmandupost' in data
+    
+    else:
+        return None, False
